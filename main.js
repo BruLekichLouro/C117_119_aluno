bc5c6524763b71360257cff194c132e32419f1c2
Bruna Lekich Louro <brunalekich@gmail.com>
2024-01-03 11:31:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+
+function setup() {
+
+}
+
+function preload() {
+
+
+  
+
+
+
+function clearCanvas() {
+
+  
+}
+
+function draw() {
+
+ 
+}
+
+function classifyCanvas() {
+  
+}
+
+function gotResult(error, results) {
+  
+}
+
+
